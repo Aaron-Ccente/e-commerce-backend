0a28bcfe68b769cfe1b69af0a7775beaf7b176cd
Aaron-Ccente <aronccente@gmail.com>
2025-07-24 22:22:03 -0500

    Configure my backend for ecommerce

---
+++ src/config/config.js
@@ -0,0 +1,7 @@
+export const PORT = process.env.PORT || 8081;
+
+export const DB_HOST = process.env.HOST || "localhost";
+export const DB_USER = process.env.USER || "root";
+export const DB_PASSWORD = process.env.PASSWORD || "Ccentejuan06@";
+export const DB_NAME = process.env.NAME || "ecommerce";
+export const DB_PORT = process.env.PORT || 3306;

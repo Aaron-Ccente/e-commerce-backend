848e7652dc9a571a49c2b9eb01319c3451367498
Aaron-Ccente <aronccente@gmail.com>
2025-07-28 23:30:58 -0500

    Endpoint con método GET para obtener todos los productos y consulta a MYSQL con Procedimientos almacenados

--- src/server.js
+++ src/server.js
@@ -12,7 +12,18 @@ app.get("/",(_,res)=>{
     res.send("Hello")
 })
 
-
+app.get("/productos", (_,res)=>{
+    const query = `CALL getAllProducts()`
+    db.query(query,(error, result)=>{
+        if(error){
+            console.log('Error al ejecutar el procedimiento ', error)
+            return res.status(500).json({ message: error.message || 'Error al obtener los productos'})
+        }
+        else{
+            return res.status(200).json({success:true, data: result[0] })
+        }
+    })
+})
 
 
 app.listen(PORT, ()=>{

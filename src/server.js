0086a72bb735d91728b584f048f2a37d2fe77f4c
Aaron-Ccente <aronccente@gmail.com>
2025-07-30 23:25:41 -0500

    Create and read for categories

--- src/server.js
+++ src/server.js
@@ -1,4 +1,4 @@
-import express from 'express'
+import express, { json } from 'express'
 import cors from 'cors'
 import db from './database/db.js'
 
@@ -25,6 +25,38 @@ app.get("/productos", (_,res)=>{
     })
 })
 
+//-----------------------------------------------------------------------------------------------
+
+//Endpoint para crear categorias
+app.post("/category",(req,res)=>{
+    const {name} = req.body;
+    const query = `CALL createCategory(?)`
+    db.query(query, [name], (err, result)=>{
+        if(err){
+            console.log('Error al ejecutar el procedimiento almacenado: ', err)
+            return res.status(500)-json({message: err.message || 'Error al crear una categoria'})
+        }
+        else{
+            return res.status(200).json({success: true, message: result[0]?.[0]?.mensaje})
+        }
+    })
+})
+
+//Obtener las categorias
+app.get("/category",(_, res)=>{
+    const query = `CALL getAllCategories()`
+    db.query(query, (err, result)=>{
+        if(err){
+            console.log('Error al ejecutar el procedimiento' || err)
+            return res.status(500).json({ message: err.message || 'Error al obtener las categorias' })
+        }
+        else{
+            return res.status(200).json(result[0])
+        }
+    })
+})
+
+//-----------------------------------------------------------------------------------------------
 
 app.listen(PORT, ()=>{
     console.log("Server listen in port: ", PORT)

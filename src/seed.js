41d0fa8fe35ea8eb2105f87ac47403d26dc19e8d
Aaron-Ccente <aronccente@gmail.com>
2025-08-09 23:11:39 -0500

    Seed para usuarios

--- src/seed.js
+++ src/seed.js
@@ -1,1 +1,17 @@
-const forniture = ` INSERT INTO forniture VALUES ('Muebles para niños', 'Muebles de alta calidad para comprar');`
+import db from "./database/db.js"
+
+const user = ` INSERT INTO user(name, lastname, email, password, image, phone, credit) VALUES 
+                               ('Aaron', 'Ccente Rojas', 'aronccente@gmail.com', 'password123', 'imagen', '+51959085189', 100.20),
+                               ('Prueba', 'Ccente Prueba', 'aronccente@gmail.com', 'password123', 'imagen', '+51959085180', 200.20)
+                               ;`
+
+
+db.query(user, (err, result) => {
+    if (err) {
+        console.log(err);
+        return 'Error al insertar datos';
+    } else {
+        console.log('Datos insertados correctamente', result);
+        return 'Datos insertados correctamente';
+    }
+});

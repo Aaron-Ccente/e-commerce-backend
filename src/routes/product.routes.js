8c9aa93751c528d1f4074ec3fa47d3eb8e9121d5
Aaron-Ccente <aronccente@gmail.com>
2025-08-10 23:18:54 -0500

    Rutas endpoints RESTful para los productos

---
+++ src/routes/product.routes.js
@@ -0,0 +1,8 @@
+import { Router } from "express";
+import { getAllProducts, createProduct, updateProduct, deleteProduct } from "../controllers/product.controller";
+
+const router = Router();
+router.get("/", getAllProducts);
+router.post("/", createProduct);
+router.put("/:id_product", updateProduct)
+router.delete("/:id_product", deleteProduct)

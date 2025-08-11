59cc7719dc0c822688d7e32ff1df969eb53fc523
Aaron-Ccente <aronccente@gmail.com>
2025-08-10 23:18:02 -0500

    Rutas para los productos

--- src/server.js
+++ src/server.js
@@ -3,6 +3,8 @@ import cors from 'cors'
 import db from './database/db.js'
 import categoryRouters from './routes/category.routes.js'
 import userRouters from './routes/user.routes.js'
+import categoryRoutes from './routes/product.routes.js'
+
 import { PORT } from './config/config.js';
 const app = express();
 app.use(cors())
@@ -25,65 +27,8 @@ app.use("/user", userRouters)
 
 //-----------------------------------------------------------------------------------------------
 
-//Create one product (name, price, image, description, amount) -> amount is required for stock table with its product
-app.post("/product", (req,res)=>{
-    const { name, price, image, description, amount } = req.body;
-    const query = `CALL createProduct(?,?,?,?,?)`;
-    db.query(query, [name, price, image, description, amount], (err, result)=>{
-        if(err){
-            console.log('Error al ejecutar el procedimiento', err)
-            return res.status(500).json({message: err.message || 'Error al crear el producto'})
-        }
-        else{
-            return res.status(200).json({success: true, message: result[0]?.[0]?.mensaje})
-        }
-    })
-})
-
-//Get all products for see in Cards
-app.get("/products", (_,res)=>{
-    const query = `CALL getAllProducts()`
-    db.query(query,(error, result)=>{
-        if(error){
-            console.log('Error al ejecutar el procedimiento ', error)
-            return res.status(500).json({ message: error.message || 'Error al obtener los productos'})
-        }
-        else{
-            return res.status(200).json({success:true, data: result[0] })
-        }
-    })
-})
-
-//Update one product (name, price, image, description, amount) -> amount is required for stock table with its product
-app.put("/product/:id_product", (req,res)=>{
-    const { id_product }  = req.params;
-    const { name, price, image, description, amount } = req.body;
-    const query = `CALL updateProduct(?,?,?,?,?,?)`;
-    db.query(query, [id_product, name, price, image, description, amount], (err, result)=>{
-        if(err){
-            console.log('Error al ejecutar el procedimiento', err)
-            return res.status(500).json({message: err.message || 'Error al actualizar el producto'})
-        }
-        else{
-            return res.status(200).json({success: true, message: result[0]?.[0]?.mensaje})
-        }
-    })
-})
-
-//Delete one product
-app.delete("/product/:id_product", (req,res)=>{
-    const { id_product } = req.params;
-    const query = `CALL deleteProduct(?)`
-    db.query(query, [id_product], (err,result)=>{
-        if(err){
-            console.log('Error al ejecutar el procedimiento almacenado', err)
-            return res.status(500).json({message: err.message || 'Error al eliminar el producto'})
-        }
-        else{
-            return res.status(200).json({success: true, message: result[0]?.[0]?.mensaje})
-        }
-    })
-})
+// Rutas para los productos
+app.use("/product", categoryRoutes)
 
 //-----------------------------------------------------------------------------------------------
 

981c06d2e658e4de028fd53e7edfb0e2e19d466f
Aaron-Ccente <aronccente@gmail.com>
2025-08-10 23:51:22 -0500

    Rutas para las ordenes de los usuarios

--- src/server.js
+++ src/server.js
@@ -6,6 +6,7 @@ import userRouters from './routes/user.routes.js'
 import categoryRoutes from './routes/product.routes.js'
 import favoriteRoutes from './routes/favorite.routes.js'
 import filterRoutes from './routes/filter.routes.js'
+import orderRoutes from './routes/order.routes.js'
 
 import { PORT } from './config/config.js';
 const app = express();
@@ -44,36 +45,8 @@ app.use("/filter", filterRoutes)
 
 //-----------------------------------------------------------------------------------------------
 
-// Get all orders of user
-app.get("/order", (req,res)=>{
-    const { id } = req.query; //http://localhost:8081/order?id=1 ejemplo :3
-    const query = `CALL getAllOrderOfUser(?)`
-    db.query(query, [id], (err, result)=>{
-        if(err){
-            console.log('Error al ejecutar el procedimiento almacenado', err)
-            return res.status(500).json({message: err.message})
-        }
-        else{
-            return res.status(200).json({success: true, data: result[0]?.[0]})
-        }
-    })
-})
-
-// Update order of user -> states
-app.put("/order/:id_order", (req,res)=>{
-    const { id_order } = req.params;
-    const { state } = req.body;
-    const query = `CALL updateStateOrderOfUser(?,?)`
-    db.query(query, [id_order, state], (err,result)=>{
-        if(err){
-            console.log('Error al ejecutar el procedimiento', err)
-            return res.status(500).json({message: err.message})
-        }
-        else{
-            return res.status(200).json({success: true, message: result[0]?.[0]?.mensaje})
-        }
-    })
-})
+// Rutas para las ordenes de los usuarios
+app.use("/order", orderRoutes)
 
 //-----------------------------------------------------------------------------------------------
 

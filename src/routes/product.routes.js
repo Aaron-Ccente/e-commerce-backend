2d5f0c46bfe0b997b1d1962ba57880a82c7c993f
Aaron-Ccente <aronccente@gmail.com>
2025-08-10 23:20:01 -0500

    Exportacion por defecto para las rutas del producto

--- src/routes/product.routes.js
+++ src/routes/product.routes.js
@@ -4,5 +4,7 @@ import { getAllProducts, createProduct, updateProduct, deleteProduct } from "../
 const router = Router();
 router.get("/", getAllProducts);
 router.post("/", createProduct);
-router.put("/:id_product", updateProduct)
-router.delete("/:id_product", deleteProduct)
+router.put("/:id_product", updateProduct);
+router.delete("/:id_product", deleteProduct);
+
+export default router;

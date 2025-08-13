45b4d22dad55304b696572298f5edc3ede939980
Aaron-Ccente <aronccente@gmail.com>
2025-08-12 22:49:30 -0500

    Comentarios para la api de las ordenes

--- src/routes/order.routes.js
+++ src/routes/order.routes.js
@@ -2,8 +2,11 @@ import { Router } from "express";
 import { getAllOrderOfUser, createOrderProductUser ,updateStateOrderOfUser } from "../controllers/order.controller.js";
 
 const router = Router();
+// Ruta para obtener todas las ordenes de un usuario
 router.get("/", getAllOrderOfUser);
+// Ruta para crear una orden de usuario
 router.post("/", createOrderProductUser);
+// Ruta para actualizar el estado de una orden de usuario
 router.put("/:id_order", updateStateOrderOfUser);
 
 export default router

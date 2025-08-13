bded5bef7c6c5d5d1a12b9bd276868588ea277d0
Aaron-Ccente <aronccente@gmail.com>
2025-08-12 22:44:34 -0500

    Comentarios para las rutas api de categorias

--- src/routes/category.routes.js
+++ src/routes/category.routes.js
@@ -2,9 +2,13 @@ import { Router } from "express";
 import { getAllCategories, createCategory, updateCategory, deleteCategory } from "../controllers/category.controller.js";
 
 const router = Router();
+// Ruta para obtener todas las categorias de los productos
 router.get("/", getAllCategories);
+// Ruta para crear categorias de productos
 router.post("/", createCategory);
+// Rtua para editar las categorias de productos
 router.put("/:id", updateCategory);
+// Rutas para eliminar categorias
 router.delete("/:id", deleteCategory)
 
 export default router;
